76cef912b38224e0ca8a6b2d19d196ada6451c6c
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-06-10 00:10:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('hello pyCharm')
+# print('hello pyCharm')
+
+def sum_pairs(ints, s):
+    resultPair = None
+    currentList = list(ints)
+
+    def iterateList(list, needSum):
+        nonlocal resultPair, currentList
+
+        if currentList:
+
+            for i in range(len(list)):
+                for j in range(i+1, len(list)):
+                    if list[i]+list[j] == needSum:
+                        resultPair = [list[i], list[j]]
+                        currentList = currentList[i+1:j]
+                        return iterateList(currentList, needSum)
+
+    iterateList(currentList, s)
+
+    return resultPair
+
+
+print(sum_pairs([10, 5, 2, 8, 3, 7, 5],10))
+
+# работает, но на больших входных списках отваливается по таймауту, сложность O(n^2)
+# переделать на сложность O(n)
