90f95b68af3fdd098d90f27f58c868bc9662bc29
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-09-04 00:13:41 +0300

    Linter hints updated

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ y = 1
 z = 2
 n = 3
 
-my_list = [[a, b, c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if sum([a,b,c]) != n]
+my_list = [[a, b, c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if sum([a, b, c]) != n]
 print(my_list)
