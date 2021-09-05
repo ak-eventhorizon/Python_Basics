6ea89f82f5d56d39d190313fbbb1dda4bd6182d9
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-09-05 13:59:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-string = 'QA2'
+# Replace all ______ with rjust, ljust or center.
 
-print(any(char.isalnum() for char in string))
-print(any(char.isalpha() for char in string))
-print(any(char.isdigit() for char in string))
-print(any(char.islower() for char in string))
-print(any(char.isupper() for char in string))
+thickness = int(input('Thickness: '))  # This must be an odd number
+c = 'H'
 
+# Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
-# In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
-# In the second line, print True if  has any alphabetical characters. Otherwise, print False.
-# In the third line, print True if  has any digits. Otherwise, print False.
-# In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
-# In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+# Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+# Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))
+
+# Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+# Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
