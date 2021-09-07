bd69d96f95b27cc64754012bc6e8edc56defd9ca
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-09-08 00:38:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def staircase(n):
-    for i in range(1, n+1):
-        stair = '#'*i
-        print(stair.rjust(n, ' '))
+def solve(s):
+    return ' '.join([word.capitalize() for word in s.split(' ')])
 
 
-staircase(6)
+print(solve('john  q  smith green 12abc'))
+print(solve('q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M'))
