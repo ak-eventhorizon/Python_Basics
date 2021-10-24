8e4bbb7439b2cc3b0b88b3db5b5fad927bc6a20b
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-10-24 14:44:15 +0300

    Create example_float_validation.py

---
+++ re/example_float_validation.py
@@ -0,0 +1,31 @@
+import re
+
+
+def floating_validator(string: str) -> bool:
+    pattern = r'^[+-]?([0-9]*[.])?[0-9]+$'
+    res = re.match(pattern, string)
+    return bool(res)
+
+
+if __name__ == '__main__':
+    print(floating_validator('+4.50'))      # True
+    print(floating_validator('-1.0'))       # True
+    print(floating_validator('.5'))         # True
+    print(floating_validator('-.7'))        # True
+    print(floating_validator('+.4'))        # True
+    print(floating_validator('+-4.5'))      # False
+    print(floating_validator('12.'))        # False
+    print(floating_validator('12.0'))       # True
+    print(floating_validator('12.0.5'))     # False
+    print(floating_validator('4.0o0'))      # False
+    print(floating_validator('-1.00'))      # True
+    print(floating_validator('+4.54'))      # True
+    print(floating_validator('Somthn'))     # False
+
+    # этот блок для выполнения упражнения на хакерранк, в гит уго помещать не нужно
+    test_cases = int(input())
+    results = list()
+    for i in range(test_cases):
+        results.append(floating_validator(input()))
+    for result in results:
+        print(result)
