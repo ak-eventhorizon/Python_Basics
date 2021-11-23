9b0f705ffa5e3bc258a2dfb46af146599e563f6a
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-11-24 00:24:23 +0300

    Create arrays.py

---
+++ numpy/arrays.py
@@ -0,0 +1,27 @@
+﻿"""
+A NumPy array is a grid of values. 
+They are similar to lists, except that every element of an array must be the same type.
+
+You are given a space separated list of numbers.
+Your task is to print a reversed NumPy array with the element type float.
+
+Input:
+1 2 3 4 -8 -10
+
+Output:
+[-10.  -8.   4.   3.   2.   1.]  -- reverse numpy array with float type
+"""
+import numpy as np
+
+
+def arrays(a: list) -> list:
+
+    a.reverse()
+    np_arr = np.array(a, float)
+    return np_arr
+
+
+if __name__ == '__main__':
+
+    arr = [1, 2, 3, 4, -8, -10]
+    print(arrays(arr))
